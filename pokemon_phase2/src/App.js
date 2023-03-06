23a27fc703106a956f320c44e0422cc21d8b9609
Anson Guo <anson.guo.921104@gmail.com>
2023-03-06 15:19:53 -0500

    completed router

--- pokemon_phase2/src/App.js
+++ pokemon_phase2/src/App.js
@@ -3,16 +3,14 @@ import CardsContainer from './components/CardsContainer';
 import DeckBuilder from './components/DeckBuilder';
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
-import {Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 
 
 function App() {
   return (
       <div>
-        <nav id="NavBar">
         <NavBar/>
-        </nav>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route path="/CardsContainer" element={<CardsContainer/>}></Route>
--- pokemon_phase2/src/components/NavBar.js
+++ pokemon_phase2/src/components/NavBar.js
@@ -4,9 +4,9 @@ import {NavLink} from "react-router-dom"
 function NavBar () {
     return (
         <nav>
-           <span><NavLink to="*">Home</NavLink></span>
-            <NavLink to="/CardsContainer">Cards</NavLink>
-            <NavLink to="/DeckBuilder">Build Your Decks</NavLink>
+           <NavLink to="*">Home</NavLink>
+           <NavLink to="/CardsContainer">Cards</NavLink>
+           <NavLink to="/DeckBuilder">Build Your Decks</NavLink>
         </nav>
     )
 }
