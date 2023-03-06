2a6fe1a18e826da02a61ef15877516443c04ae0f
Jacob Park <jacobypark122@gmail.com>
2023-03-06 15:10:38 -0500

    fixed routes

--- pokemon_phase2/src/App.js
+++ pokemon_phase2/src/App.js
@@ -3,7 +3,7 @@ import CardsContainer from './components/CardsContainer';
 import DeckBuilder from './components/DeckBuilder';
 import NavBar from "./components/NavBar"
 import Home from "./components/Home"
-import {Route} from "react-router-dom"
+import {Route, Routes} from "react-router-dom"
 
 
 
@@ -13,16 +13,11 @@ function App() {
         <nav id="NavBar">
         <NavBar/>
         </nav>
-       
-        <Route path="*">
-        <Home />
-        </Route>
-        <Route path="/CardsContainer">
-        <CardsContainer/>
-        </Route>
-        <Route path="DeckBuilder">
-        <DeckBuilder />
-        </Route>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/CardsContainer" element={<CardsContainer/>}></Route>
+          <Route path="DeckBuilder" element={<DeckBuilder />}></Route>
+        </Routes>
       
       </div>
   );
