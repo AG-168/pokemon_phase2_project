8ebd9b5cfdc03db58c6475cf8eb84e20a5be4748
Jacob Park <jacobypark122@gmail.com>
2023-03-06 14:42:38 -0500

    added nav bar

--- pokemon_phase2/src/App.js
+++ pokemon_phase2/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <nav className="navbar">
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/about">
+          About
+        </Link>
+      </nav>
     </div>
   );
 }
