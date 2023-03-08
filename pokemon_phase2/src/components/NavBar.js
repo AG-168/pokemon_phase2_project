ffea6e61a21af5f7c770bd3d9ed65090d761ff53
Jacob Park <jacobypark122@gmail.com>
2023-03-08 16:11:28 -0500

    add click handler

--- pokemon_phase2/src/components/NavBar.js
+++ pokemon_phase2/src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar ({onHandleCardsClick}) {
     return (
         <nav id="navBar">
            <NavLink to="*" className="link"> Home</NavLink>
-           <NavLink to="/CardsContainer" className="link"> Cards</NavLink>
+           <NavLink to="/CardsContainer" className="link" onClick={handleCardsClick}> Cards</NavLink>
            <NavLink to="/DeckBuilder" className="link"> Build Your Decks</NavLink>
            <NavLink to="/Login" className="link"> Login</NavLink>
            <NavLink to="/SignUp" className="link"> SignUp</NavLink>
